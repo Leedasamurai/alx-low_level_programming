0bc227f2b77488016e85a1a459bb20e692e7f369
Leedasamurai <lesegoleemac@gmail.com>
2023-07-14 13:17:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
 
