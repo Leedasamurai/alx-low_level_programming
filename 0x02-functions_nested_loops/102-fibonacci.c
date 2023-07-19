dc81f3f498db9ec65be03578bca394f42ee92ebd
Leedasamurai <lesegoleemac@gmail.com>
2023-07-18 17:58:31 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print fibonacci sequence starting from 1 and 2 to 50 numbers.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
