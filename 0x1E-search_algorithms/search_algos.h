9fb1aa6aa3f9febb1530d566fa4853cd701cd98c
Leedasamurai <lesegoleemac@gmail.com>
2024-03-14 01:16:59 -0700

    location fix

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
+
+#endif
